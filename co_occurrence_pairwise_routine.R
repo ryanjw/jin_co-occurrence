e28ddd86b96c38c14553779177f4e61e58dbc1cc
Ryan Williams <ryanwilliams@x-134-84-202-148.uofm-secure.wireless.umn.edu>
2015-02-25 09:59:19 -0600

    edited pairwise routine, should work now

--- co_occurrence_pairwise_routine.R
+++ co_occurrence_pairwise_routine.R
@@ -29,32 +29,25 @@ for(a in 1:length(trts)){
 			species1.ab<-sum(temp[,b])
 			species2.ab<-sum(temp[,c])
 			#if the column is all 0's no co-occurrence will be performed
-			if(species1.ab >1 & species2.ab >1){
+			if(species1.ab >0 & species2.ab >0){
 				test<-cor.test(temp[,b],temp[,c],method="spearman",na.action=na.rm)
 				rho<-test$estimate
 				p.value<-test$p.value
 			}
 			
-			if(species1.ab <=1 | species2.ab <= 1){
+			if(species1.ab <=0 | species2.ab <= 0){
 				rho<-0
 				p.value<-1
 			}	
 			
 			new.row<-c(trts[a],names(temp)[b],names(temp)[c],rho,p.value,species1.ab,species2.ab)
 			results<-rbind(results,new.row)			
-			
+			print(paste(c/dim(temp)[2]*100,"% Done of Treatment ",trts[a],sep=""))
 		}
 		
-	}
-	
-	
-	print(a/length(trts))
-	
+	}	
 }
 
-
-head(results)
-results<-data.frame(data.matrix(results))
 names(results)<-c("trt","taxa1","taxa2","rho","p.value","ab1","ab2")
 
 #Because output from MG-RAST has some eukaryotic orders and families they must be removed before futher analysis.  These groups are therefore remmoved with the following subetting statements
